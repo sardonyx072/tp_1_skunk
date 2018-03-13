5410d757cf80f584467c30b245bcdf9889debed7
sardonyx072 <mithoffman11@gmail.com>
2018-03-13 17:31:03 -0500

    bot names json generator, need a json parser to generate random bot name

--- Skunk/src/main/BotPlayer.java
+++ Skunk/src/main/BotPlayer.java
@@ -5,13 +5,19 @@ import java.util.UUID;
 public abstract class BotPlayer extends Player {
 	private static final String[] NAMES = new String[]{
 			// scientists
-			"Albert Einstein","Nikola Tesla","Marie Curie","Euclid","Thomas Edison","Bill Nye","Max Planck",
+			"Albert Einstein","Nikola Tesla","Marie Curie","Euclid","Thomas Edison","Max Planck","Avogadro","Isaac Newton","Galileo Galilei","Charles Darwin","Stephen Hawking","Michael Faraday","Nicolaus Copernicus",
+			"Louis Pasteur","Leonardo da Vinci","Alexander Graham Bell","Francis Crick","James Watson","Archimedes","Rosalind Franklin","Niels Bohr","Gregor Mendel","George Washington Carver","James Clerk Maxwell",
+			"Johannes Kepler","Richard Feynman","Ernest Rutherford","Robert Boyle","Rachel Carson","Alexander Fleming","Ada Lovelace","Guglielmo Marconi","Carl Sagan","Alessandro Volta","Pierre Curie","Antoine Lavoisier",
+			"Tim Berners-Lee","Robert Hooke","Edwin Hubble","Mary Anning","William Thomson","Alfred Nobel","James Prescott Joule","Blaise Pascal","Enrico Fermi","Linus Pauling","John Dalton","Dmitri Mendeleev",
+			"Antoine van Leeuwenhoek","Erwin Schroedinger","Paul Dirac","J.J. Thomson","Ernest Walton","Franz Mesmer","Phillipe Pinel","Rudolph Virchow","Irene Joliot-Curie",
+			// philosophers
+			"Sophocles","Aristotle","",
 			// computer scientists
 			"Linus Torvalds",
 			// DnD 
-			"Dave Arneson","Gary Gygax","Mordenkainen","Bigby","Vecna","Kas","Otiluke","Tasha","Melf",
+			"Dave Arneson","Gary Gygax","Mordenkainen","Vecna","Kas","Otiluke","Strahd von Zarovich","Acererak",
 			// Marvel/DC
-			"Charles Xavier","Max Eisenhardt","Hank McCoy","Bruce Banner","Tony Stark","JARVIS","Loki","Ultron","Thanos","Dr. Octopus","Mr. Fantastic","Vision","Dr. Doom","Bruce Wayne",
+			"Charles Xavier","Bruce Banner","Tony Stark","JARVIS","Loki","Ultron",
 			// star trek
 			"Data","Lore","Locutus","Q",
 	};
