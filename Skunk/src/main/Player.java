bc084a21eb43874f571c6772c76574e7a385f1a8
sardonyx072 <mithoffman11@gmail.com>
2018-03-17 08:27:43 -0500

    removed person, largely unnecessary

--- Skunk/src/main/Player.java
+++ Skunk/src/main/Player.java
@@ -2,14 +2,22 @@ package main;
 
 import java.util.UUID;
 
-public class Player extends Person {
+public class Player {
+	protected UUID uuid;
+	protected String name;
 	
 	public Player(String name) {
 		this(name,UUID.randomUUID());
 	}
 	public Player(String name, UUID uuid) {
-		super(name, uuid);
+		if (name == null) throw new NullPointerException("Person name cannot be null");
+		if (name.isEmpty()) throw new IllegalArgumentException("Person name cannot be empty");
+		this.name = name;
+		this.uuid = uuid;
 	}
+	public UUID getUUID() {return this.uuid;}
+	public String getName() {return this.name;}
+	public void setName(String name) {this.name = name;}
 	public String toString() {return "{"+this.getName()+","+this.getUUID()+"}";}
 	// TODO stats?
 }
--- Skunk/src/test/PlayerTest.java
+++ Skunk/src/test/PlayerTest.java
@@ -3,48 +3,36 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import main.Person;
 import main.Player;
 
 public class PlayerTest {
-	
-	@SuppressWarnings("unused")
-	@Test
-	public void testIsPerson() {
-		boolean test = false;
-		try {
-			Person person = new Player("A");
-			test = true;
-		} catch (Exception e) {}
-		assertTrue("Player can be assigned to a Person alias",test);
-	}
 
 	@SuppressWarnings("unused")
 	@Test
 	public void testName() {
 		boolean test = true;
 		try {
-			Person person = new Player(null);
+			Player person = new Player(null);
 			test = false;
 		} catch (Exception e) {}
 		assertTrue("Person throws exception if name is null",test);
 		
 		test = true;
 		try {
-			Person person = new Player("");
+			Player person = new Player("");
 			test = false;
 		} catch (Exception e) {}
 		assertTrue("Person throws exception if name is empty",test);
 		
 		test = false;
 		try {
-			Person person = new Player("A");
+			Player person = new Player("A");
 			test = true;
 		} catch (Exception e) {}
 		assertTrue("Person can have one character name",test);
 		
 		String name = "TestName123";
-		Person person = new Player(name);
+		Player person = new Player(name);
 		assertEquals("Person name matches expected",name,person.getName());
 	}
 }
